37363866b600edf70d5037a7937bf44519d2ba61
kajal6123 <kajalrajadx@gmail.com>
2026-09-23 06:14:12 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/ojalacrade/mazepets/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ojalacrade.mazepets;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/maze-pets.html");
+    }
+}
